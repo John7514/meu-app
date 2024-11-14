b638ea796881674003bd2821d287447b16d4ebe6
John7514 <jjoaovictorpercussao@gmail.com>
2024-11-14 12:26:51 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import { people } from './data.js'; 
+import { getImageUrl } from './utils.js';
 
+export default function List() {
+  // Não filtrar as pessoas, exibir todas
+  const listItems = people.map(person => 
+    <li key={person.id}> {/* Adicionei a key aqui para evitar warnings */}
+      <img 
+        src={getImageUrl(person)} 
+        alt={person.name} 
+        onError={(e) => e.target.src = 'default-image.jpg'} // Isso pode ser útil caso a imagem falhe
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
+
+  return <ul>{listItems}</ul>;
+}

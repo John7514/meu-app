e377dd9ca34783495ae85c9936e78069035d8033
John7514 <jjoaovictorpercussao@gmail.com>
2024-11-21 11:42:28 -0300

    minhasRotas.js

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Atv01 from "./Atv01";
 import Atv02 from "./Atv02";
 import App from "./App";
+import App2 from "./App2";
 
 export default function MinhasRotas(){
     return(
@@ -13,6 +14,7 @@ export default function MinhasRotas(){
             <Route path="/Atv01" element={<Atv01 />} />
             <Route path="/Atv02" element={<Atv02 />} />
             <Route path="/App" element={<App />} />
+             <Route path="/App2" element={<App2 />} />
         </Routes>
         </BrowserRouter>
     );
